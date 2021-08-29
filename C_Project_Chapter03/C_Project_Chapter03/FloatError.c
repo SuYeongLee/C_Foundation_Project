99e8309fe6502388b7917afa45dad2ef6cc0a213
SuYeongLee <rainbo3700@gmail.com>
2021-08-29 22:09:26 +0900

    Chapter03

---
+++ C_Project_Chapter03/C_Project_Chapter03/FloatError.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	float num = 0.0;
+
+	for (i = 0; i < 100; i++)
+	{
+		num += 0.1; // 이 연산을 총 100회 진행하게 됨.
+
+		printf("i값 : %d \n", i);
+		printf("num값 : %f \n", num);
+	}
+
+	printf("0.1을 100번 더한 결과 : %f \n", num);
+	return 0;
+}
