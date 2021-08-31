c73ed46ab63ecd7527f528de9d3fa2a19faba8ee
SuYeongLee <rainbo3700@gmail.com>
2021-08-31 23:25:41 +0900

    Chapter08

---
+++ C_Project_Chapter08/C_Project_Chapter08/LocalValHideGlobalVal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 5;
+	printf("num: %d \n", Add(3));
+	printf("num: %d \n", num + 9);
+	return 0;
+}
+
+int Add(int val)
+{
+	int num = 9;
+	num += val;
+	return num;
+}
