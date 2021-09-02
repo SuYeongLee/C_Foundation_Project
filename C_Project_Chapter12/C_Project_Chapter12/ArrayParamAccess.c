8bb2d03205f87cb5fc4babd08a54fbacd6efb107
SuYeongLee <rainbo3700@gmail.com>
2021-09-02 19:53:22 +0900

    Capter12

---
+++ C_Project_Chapter12/C_Project_Chapter12/ArrayParamAccess.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void ShowArayElen(int* param, int len)
+{
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		printf("%d ", param[i]);
+	}
+	printf("\n");
+}
+
+void AddArayElem(int* param, int len, int add)
+{
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		param[i] += add;
+	}
+}
+
+int main(void)
+{
+	int arr[3] = { 1, 2, 3 };
+	AddArayElem(arr, sizeof(arr) / sizeof(int), 1);
+	ShowArayElen(arr, sizeof(arr) / sizeof(int));
+
+	AddArayElem(arr, sizeof(arr) / sizeof(int), 2);
+	ShowArayElen(arr, sizeof(arr) / sizeof(int));
+
+	AddArayElem(arr, sizeof(arr) / sizeof(int), 3);
+	ShowArayElen(arr, sizeof(arr) / sizeof(int));
+
+	return 0;
+}
